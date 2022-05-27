032d0e843833291d6e5073290017d970f0146b1c
Lesmiscore <nao20010128@gmail.com>
2022-05-27 18:18:38 +0900

    remove all python things

--- build_cgo.sh
+++ build_cgo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
 # main.go is already excluded using https://pkg.go.dev/cmd/go#hdr-Build_constraints
-CGO_ENABLED=1 go build -buildmode=c-shared -o ytarchive.so
+CGO_ENABLED=1 go build -buildmode=c-shared -o _ytarchive.so
 python3 test_yta.py
-python3 -c 'import ytarchive; print(ytarchive.sum(8,9))'
